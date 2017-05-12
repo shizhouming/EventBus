0b31a6e1e61a27fc06155ce6bc929951f5774497
shizhouming <513474472@qq.com>
2017-05-12 16:59:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/eventbus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.eventbus;
+
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
